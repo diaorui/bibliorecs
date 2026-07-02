9fc4dcd50df82b971cea59c0b348400c1f91bdcc
diaorui <diaorui1987@gmail.com>
2026-07-02 09:27:38 -0700

    Filter English books during recommendation generation

--- recommend.py
+++ recommend.py
@@ -102,7 +102,7 @@ def compute(conn):
     books = conn.execute("""
         SELECT metadata_id, call_number
         FROM books WHERE active = 1
-    """).fetchall()
+    """ + (" AND primary_language = 'eng'" if config.FILTER_ENGLISH else "")).fetchall()
     books = [dict(r) for r in books]
 
     by_cat = defaultdict(list)
